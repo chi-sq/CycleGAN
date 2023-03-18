5581e45268e55726b8a48a10c82cb47152294b01
chi-sq <1093564551@qq.com>
2023-03-18 14:36:58 +0800

    CycleGAN

---
+++ generator_model.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, down=True, use_act=True, **kwargs):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, padding_mode="reflect", **kwargs)
+            if down
+            else nn.ConvTranspose2d(in_channels, out_channels, **kwargs),
+            nn.InstanceNorm2d(out_channels),
+            nn.ReLU(inplace=True) if use_act else nn.Identity()
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.block = nn.Sequential(
+            ConvBlock(channels, channels, kernel_size=3, padding=1),
+            ConvBlock(channels, channels, use_act=False, kernel_size=3, padding=1),
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
+
+class Generator(nn.Module):
+    def __init__(self, img_channels, num_features = 64, num_residuals=9):
+        super().__init__()
+        self.initial = nn.Conv2d(img_channels, num_features, kernel_size=7, stride=1, padding=3, padding_mode="reflect")
+            # nn.InstanceNorm2d(num_features),
+            # nn.ReLU(inplace=True),
+
+        # nn.ModuleList 为什么不用 nn.Sequential
+        self.down_blocks = nn.ModuleList(
+            [
+                ConvBlock(num_features, num_features*2, kernel_size=3, stride=2, padding=1),
+                ConvBlock(num_features*2, num_features*4, kernel_size=3, stride=2, padding=1),
+            ]
+        )
+        self.res_blocks = nn.Sequential(
+            *[ResidualBlock(num_features*4) for _ in range(num_residuals)]
+        )
+        self.up_blocks = nn.ModuleList(
+            [
+                # output_padding :additional padding after convBlock   search for output_padding
+                ConvBlock(num_features*4, num_features*2, down=False, kernel_size=3, stride=2, padding=1, output_padding=1),
+                ConvBlock(num_features*2, num_features*1, down=False, kernel_size=3, stride=2, padding=1, output_padding=1),
+            ]
+        )
+
+        self.last = nn.Conv2d(num_features*1, img_channels, kernel_size=7, stride=1, padding=3, padding_mode="reflect")
+
+    def forward(self, x):
+        x = self.initial(x)
+        # x = self.down_blocks(x) # 为什么下面的代码不直接传导 , 因为用的是nn.ModuleList而不是nn.Sequential
+        for layer in self.down_blocks:
+            x = layer(x)
+        x = self.res_blocks(x)
+        # x = self.up_blocks(x)
+        for layer in self.up_blocks:
+            x = layer(x)
+        return torch.tanh(self.last(x))  # 使得像素值 between [-1，1]
+
+def test():
+    img_channels = 3
+    img_size = 256
+    x = torch.randn((2, img_channels, img_size, img_size))
+    gen = Generator(img_channels, 9)
+    print(gen)
+    print(gen(x).shape)
+
+if __name__ == "__main__":
+    test()
